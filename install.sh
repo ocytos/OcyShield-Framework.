12f4b5f3cc9a3f75f5f608d317955ba6fd53c76d
ocy <ocy@ocy.ocy>
2026-04-09 21:49:05 -0700

    Update install.sh for remote curl installation

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 RED='\033[0;31m'
 NC='\033[0m'
-echo -e "${RED}[*] Configurando OcyShield como comando global...${NC}"
-DIR_ACTUAL=$(pwd)
-# Creamos el lanzador en /usr/local/bin
-echo -e "#!/bin/bash\ncd $DIR_ACTUAL && python3 main.py \"\$@\"" | sudo tee /usr/local/bin/ocysh > /dev/null
+
+echo -e "${RED}[*] OcyShield Framework - Installing...${NC}"
+
+# Si la carpeta no existe, la clonamos en /opt
+if [ ! -d "/opt/ocyshield" ]; then
+    echo -e "${RED}[*] Cloning repository to /opt/ocyshield...${NC}"
+    sudo git clone https://github.com/ocytos/OcyShield-Framework.git /opt/ocyshield
+fi
+
+# Creamos el comando global
+echo -e "#!/bin/bash\ncd /opt/ocyshield && python3 main.py \"\$@\"" | sudo tee /usr/local/bin/ocysh > /dev/null
 sudo chmod +x /usr/local/bin/ocysh
-chmod +x main.py
-echo -e "${RED}[+] LISTO! Ahora puedes usar 'ocysh' en cualquier terminal.${NC}"
+
+# Instalamos dependencias
+echo -e "${RED}[*] Installing dependencies...${NC}"
+sudo pip3 install -r /opt/ocyshield/requirements.txt --quiet
+
+echo -e "${RED}[+] Installation finished! Type 'ocysh' to start.${NC}"
